26168bd5ea3fbdd26de00075cfa08c5fd9836990
6dean <ferdjallahrockdean@gmail.com>
2022-11-07 15:42:58 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import axios from "axios";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  const [data, setData] = useState({});
+  const [isLoading, setIsLoading] = useState(true);
+
+  const fetchData = async () => {
+    const response = await axios.get("http://localhost:3100/");
+    setData(response.data);
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  return isLoading ? (
+    <span>En cours de chargement... </span>
+  ) : (
+    <>
+      <h2>{data.meta.metatags.title}</h2>
+      <p>{data.meta.metatags.titleSocial}</p>
+      <p>{data.meta.metatags.description}</p>
+      <p>{data.meta.metatags.descriptionSocial}</p>
+      <img src={data.meta.metatags.image} alt="" />
+      <p>{data.meta.restaurant.name}</p>
+    </>
   );
 }
 
